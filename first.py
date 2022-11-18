4669f29a5fbc833f0785db4d4775ec8f3a4fcd18
Anukul Srivastava <anukulsrivastava123@gmail.com>
2022-11-18 15:53:40 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,17 @@
+print("Welcome to Python Programming")
+print(10+20)
+print("2+2")
+
+a= 10
+b= 10
+print(type(a)) # this is to print the type of variable
+print(id(a)) # this is to print the address of a variable
+
+print(type(a))
+print(id(b))
+
+a,b,c = 10,"Python",2.5
+
+print(a)
+print(b)
+print(c)
